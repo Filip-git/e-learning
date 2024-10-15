084180f9e91b1b928b5c621b51f96e65b611ad2c
Filip Oroz <filip.oroz9@gmail.com>
2024-10-15 19:23:45 +0200

    small fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ const Footer = () => {
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
-          © {currentDate} <Link to="/" className="hover:underline">E-learning </Link>| PZI
+          © {currentDate} <Link to="/" className="hover:underline">E-learning </Link>
         </span>
       </div>
     </footer>
